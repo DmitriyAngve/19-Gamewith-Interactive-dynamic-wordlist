445e117d661b5ae6d5dee05b64380ee54ab174ec
Dmitriy Angve <kirirch129@gmail.com>
2023-01-27 17:18:30 +0300

    #

---
+++ app.js
@@ -0,0 +1,13 @@
+// const id = "1Ba8d3_QqLnZf7ciyT8QLoOlPkKq6mj_V7jBYooAu0jw";
+const id =
+  "2PACX-1vTo_820UbGEUwnghQY3KplOO2xH1bNKWIr59QGFFDLwipn6idxXGQAYmQtEabBQXw45w5q6TPea-p0l";
+// const url =
+//   "https://spreadsheets.google.com/feeds/list/" +
+//   id +
+//   "/1/public/values?alt=json";
+
+// ("https://spreadsheets.google.com/feeds/cells/1Ba8d3_QqLnZf7ciyT8QLoOlPkKq6mj_V7jBYooAu0jw/1/public/full?alt=json");
+
+const url =
+  "https://spreadsheets.google.com/feeds/cells/1Ba8d3_QqLnZf7ciyT8QLoOlPkKq6mj_V7jBYooAu0jw/1/public/full?alt=json";
+console.log(url);
