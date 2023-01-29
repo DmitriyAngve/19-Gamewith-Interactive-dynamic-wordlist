164d543846958d6164846afcbaa56984798ed483
Dmitriy Angve <kirirch129@gmail.com>
2023-01-29 11:23:32 +0300

    #

--- app.js
+++ app.js
@@ -41,22 +41,50 @@
 // //     console.log(JSON.stringify(data));
 // //   });
 
-const id = "1Ba8d3_QqLnZf7ciyT8QLoOlPkKq6mj_V7jBYooAu0jw";
-const url =
-  "https://spreadsheets.google.com/feeds/list/" +
-  id +
-  "/1/public/values?alt=json";
-console.log(url);
+//https://docs.google.com/spreadsheets/d/1o4vIdjHUePyU90AAdjBTblO73Hn-de1sU3_KnBHr5l8/edit?usp=sharing
+
+const id = "1o4vIdjHUePyU90AAdjBTblO73Hn-de1sU3_KnBHr5l8";
+const myWords = ["Hello World", "JavaScript Code"];
 const output = document.querySelector(".output");
 const btn = document.createElement("button");
 btn.textContent = "Load Sheet by ID";
-const sheedID = document.createElement("input");
-sheedID.setAttribute("type", "text");
-sheedID.value = id;
+const sheetID = document.createElement("input");
+sheetID.setAttribute("type", "text");
+sheetID.value = id;
 
-output.append(sheedID);
+output.append(sheetID);
 output.append(btn);
 
 btn.addEventListener("click", (e) => {
-  console.log(sheedID.value);
+  console.log(sheetID.value);
+  const url =
+    "https://spreadsheets.google.com/feeds/list/" +
+    sheetID.value +
+    "/1/public/values?alt=json";
+  btn.disabled = true;
+  console.log(url);
+
+  const div = document.createElement("div");
+  output.append(div);
+
+  fetch(url)
+    .then((req) => req.json())
+    .then((json) => {
+      console.log(json["feed"]["entry"]);
+      myWords.length = 0;
+      let enty = json.feed.entry;
+      enty.forEach((el) => {
+        console.log(el.title["$t"]);
+        // let temp = el["gsx$word"]["$t"];
+        let temp = el.title["$t"];
+        console.log(temp.includes(":"));
+        if (temp.length > 0 && !temp.includes(":")) {
+          myWords.push(temp);
+        }
+      });
+      console.log(myWords);
+    })
+    .catch((err) => {
+      console.log("List not loaded");
+    });
 });
